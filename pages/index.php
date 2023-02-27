3bca9dba460f1879954b619078d12a05253e6134
MyLifeForUkraine <a.kirichenko2312@gmail.com>
2023-02-27 04:49:08 +0200

    added index.php page - our main page

---
+++ pages/index.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Ultramarine</title>
+   <link rel='stylesheet' href='../css/index.css'>
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+</head>
+
+<body>
+   <div class="wrapper">
+      <div class="container">
+         <header class="header">
+            <div class="header__logo">
+               <img src="../static/svg/logo.svg" alt="">
+            </div>
+            <div class="header__search search-header">
+               <form action="" class="search-header__form">
+                  <input type="text" class="search-header__input">
+               </form>
+               <img src="../static/svg/magnifying-glass.svg" alt="">
+            </div>
+            <div class="header__pages">
+               <div class="header__page">
+                  <a href="#" class="header__link-to-page">
+                     Головна
+                  </a>
+               </div>
+               <div class="header__page">
+                  <a href="#" class="header__link-to-page">
+                     Каталог
+                  </a>
+               </div>
+               <div class="header__page">
+                  <a href="#" class="header__link-to-page">
+                     <img src="../static/svg/favourite.svg" alt="">
+                  </a>
+               </div>
+               <div class="header__page">
+                  <a href="#" class="header__link-to-page">
+                     <img src="../static/svg/basket.svg" alt="">
+                  </a>
+               </div>
+            </div>
+         </header>
+         <div class="bestsellers">
+            <div class="bestsellers__title">
+               Найбільш популярні книги
+            </div>
+            <div class="bestsellers__body">
+               <div class="bestsellers__item item-bestsellers">
+                  <div class="item-bestsellers__body">
+                     <a href="">
+                        <img class='item-bestsellers__bookimage' src="../static/jpeg/Internat.jpeg" alt="">
+                     </a>
+                     <img class='item-bestsellers__favourite' src="../static/svg/favourite-empty.svg" alt="">
+                     <a class="item-bestsellers__title" href="">
+                        Інтернат
+                     </a>
+                     <div class="item-bestsellers__author">
+                        Сергій Жадан
+                     </div>
+                     <div class="item-bestsellers__price">
+                        235 грн
+                     </div>
+                     <div class="item-bestsellers__buttons">
+                        <a class="item-bestsellers__decription item-bestsellers__button" href="">
+                           Опис
+                        </a>
+                        <a class="item-bestsellers__buy item-bestsellers__button" href="">
+                           Купити
+                        </a>
+                     </div>
+
+                  </div>
+               </div>
+               <div class="bestsellers__item item-bestsellers">
+                  <div class="item-bestsellers__body">
+                     <a href="">
+                        <img class='item-bestsellers__bookimage' src="../static/jpeg/Internat.jpeg" alt="">
+                     </a>
+                     <img class='item-bestsellers__favourite' src="../static/svg/favourite-empty.svg" alt="">
+                     <a class="item-bestsellers__title" href="">
+                        Інтернат
+                     </a>
+                     <div class="item-bestsellers__author">
+                        Сергій Жадан
+                     </div>
+                     <div class="item-bestsellers__price">
+                        235 грн
+                     </div>
+                     <div class="item-bestsellers__buttons">
+                        <a class="item-bestsellers__decription item-bestsellers__button" href="">
+                           Опис
+                        </a>
+                        <a class="item-bestsellers__buy item-bestsellers__button" href="">
+                           Купити
+                        </a>
+                     </div>
+
+                  </div>
+               </div>
+               <div class="bestsellers__item item-bestsellers">
+                  <div class="item-bestsellers__body">
+                     <a href="">
+                        <img class='item-bestsellers__bookimage' src="../static/jpeg/Internat.jpeg" alt="">
+                     </a>
+                     <img class='item-bestsellers__favourite' src="../static/svg/favourite-empty.svg" alt="">
+                     <a class="item-bestsellers__title" href="">
+                        Інтернат
+                     </a>
+                     <div class="item-bestsellers__author">
+                        Сергій Жадан
+                     </div>
+                     <div class="item-bestsellers__price">
+                        235 грн
+                     </div>
+                     <div class="item-bestsellers__buttons">
+                        <a class="item-bestsellers__decription item-bestsellers__button" href="">
+                           Опис
+                        </a>
+                        <a class="item-bestsellers__buy item-bestsellers__button" href="">
+                           Купити
+                        </a>
+                     </div>
+
+                  </div>
+               </div>
+               <div class="bestsellers__item item-bestsellers">
+                  <div class="item-bestsellers__body">
+                     <a href="">
+                        <img class='item-bestsellers__bookimage' src="../static/jpeg/Internat.jpeg" alt="">
+                     </a>
+                     <img class='item-bestsellers__favourite' src="../static/svg/favourite-empty.svg" alt="">
+                     <a class="item-bestsellers__title" href="">
+                        Інтернат
+                     </a>
+                     <div class="item-bestsellers__author">
+                        Сергій Жадан
+                     </div>
+                     <div class="item-bestsellers__price">
+                        235 грн
+                     </div>
+                     <div class="item-bestsellers__buttons">
+                        <a class="item-bestsellers__decription item-bestsellers__button" href="">
+                           Опис
+                        </a>
+                        <a class="item-bestsellers__buy item-bestsellers__button" href="">
+                           Купити
+                        </a>
+                     </div>
+
+                  </div>
+               </div>
+               <div class="bestsellers__item item-bestsellers">
+                  <div class="item-bestsellers__body">
+                     <a href="">
+                        <img class='item-bestsellers__bookimage' src="../static/jpeg/Internat.jpeg" alt="">
+                     </a>
+                     <img class='item-bestsellers__favourite' src="../static/svg/favourite-empty.svg" alt="">
+                     <a class="item-bestsellers__title" href="">
+                        Інтернат
+                     </a>
+                     <div class="item-bestsellers__author">
+                        Сергій Жадан
+                     </div>
+                     <div class="item-bestsellers__price">
+                        235 грн
+                     </div>
+                     <div class="item-bestsellers__buttons">
+                        <a class="item-bestsellers__decription item-bestsellers__button" href="">
+                           Опис
+                        </a>
+                        <a class="item-bestsellers__buy item-bestsellers__button" href="">
+                           Купити
+                        </a>
+                     </div>
+
+                  </div>
+               </div>
+               <div class="bestsellers__item item-bestsellers">
+                  <div class="item-bestsellers__body">
+                     <a href="">
+                        <img class='item-bestsellers__bookimage' src="../static/jpeg/Internat.jpeg" alt="">
+                     </a>
+                     <img class='item-bestsellers__favourite' src="../static/svg/favourite-empty.svg" alt="">
+                     <a class="item-bestsellers__title" href="">
+                        Інтернат
+                     </a>
+                     <div class="item-bestsellers__author">
+                        Сергій Жадан
+                     </div>
+                     <div class="item-bestsellers__price">
+                        235 грн
+                     </div>
+                     <div class="item-bestsellers__buttons">
+                        <a class="item-bestsellers__decription item-bestsellers__button" href="">
+                           Опис
+                        </a>
+                        <a class="item-bestsellers__buy item-bestsellers__button" href="">
+                           Купити
+                        </a>
+                     </div>
+
+                  </div>
+               </div>
+               <div class="bestsellers__item item-bestsellers">
+                  <div class="item-bestsellers__body">
+                     <a href="">
+                        <img class='item-bestsellers__bookimage' src="../static/jpeg/Internat.jpeg" alt="">
+                     </a>
+                     <img class='item-bestsellers__favourite' src="../static/svg/favourite-empty.svg" alt="">
+                     <a class="item-bestsellers__title" href="">
+                        Інтернат
+                     </a>
+                     <div class="item-bestsellers__author">
+                        Сергій Жадан
+                     </div>
+                     <div class="item-bestsellers__price">
+                        235 грн
+                     </div>
+                     <div class="item-bestsellers__buttons">
+                        <a class="item-bestsellers__decription item-bestsellers__button" href="">
+                           Опис
+                        </a>
+                        <a class="item-bestsellers__buy item-bestsellers__button" href="">
+                           Купити
+                        </a>
+                     </div>
+
+                  </div>
+               </div>
+               <div class="bestsellers__item item-bestsellers">
+                  <div class="item-bestsellers__body">
+                     <a href="">
+                        <img class='item-bestsellers__bookimage' src="../static/jpeg/Internat.jpeg" alt="">
+                     </a>
+                     <img class='item-bestsellers__favourite' src="../static/svg/favourite-empty.svg" alt="">
+                     <a class="item-bestsellers__title" href="">
+                        Інтернат
+                     </a>
+                     <div class="item-bestsellers__author">
+                        Сергій Жадан
+                     </div>
+                     <div class="item-bestsellers__price">
+                        235 грн
+                     </div>
+                     <div class="item-bestsellers__buttons">
+                        <a class="item-bestsellers__decription item-bestsellers__button" href="">
+                           Опис
+                        </a>
+                        <a class="item-bestsellers__buy item-bestsellers__button" href="">
+                           Купити
+                        </a>
+                     </div>
+
+                  </div>
+               </div>
+            </div>
+            <button class="bestsellers__button">
+               <a class="bestsellers__link">
+                  Catalog
+               </a>
+            </button>
+         </div>
+         <footer class="footer">
+            <div class="footer__body">
+               <div class="footer__logo logo-footer">
+                  <img src="../static/svg/logo-white.svg" alt="">
+                  <div class="logo-footer__text">
+                     Ultramarine
+                  </div>
+               </div>
+               <div class="footer__social social-footer">
+                  <p class='social-footer__text'>Зв'язатися з нами</p>
+                  <div class="social-footer__element">
+                     <a target="_blank" href="https://t.me/MyLifeForUkraine">
+                        <img class="footer__qr" src="../static/svg/telegram-qr.svg">
+                     </a>
+                  </div>
+               </div>
+               <div class="footer__navigation navigation-footer">
+                  <ul class="navigation-footer__list">
+                     <li class="navigation-footer__item item-navigation-footer">
+                        <a class="item-navigation-footer__link" href="MainPage.html">Головна</a>
+                     </li>
+                     <li class="navigation-footer__item item-navigation-footer">
+                        <a class="item-navigation-footer__link" href="MenuPage.html">Каталог</a>
+                     </li>
+                     <li class="navigation-footer__item item-navigation-footer">
+                        <a class="item-navigation-footer__link" href="BlogPage.html">Бажані</a>
+                     </li>
+                     <li class="navigation-footer__item item-navigation-footer">
+                        <a class="item-navigation-footer__link" href="ContactsPage.html">Корзина</a>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+         </footer>
+      </div>
+   </div>
+</body>
+
+</html>
